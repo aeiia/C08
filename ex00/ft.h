48431825b2981d81b834a65d2140073811df696c
tiida <takehi.iida@gmail.com>
2025-12-12 00:04:33 +0900

    ex00 ft.h

--- ex00/ft.h
+++ ex00/ft.h
@@ -6,12 +6,17 @@
 /*   By: tiida <tiida@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 22:34:04 by tiida             #+#    #+#             */
-/*   Updated: 2025/12/11 22:34:07 by tiida            ###   ########.fr       */
+/*   Updated: 2025/12/12 00:04:04 by tiida            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
 
 void	ft_putchar(char c);
 void	ft_swap(int *a, int *b);
 void	ft_putstr(char *str);
 int		ft_strlen(char *str);
 int		ft_strcmp(char *s1, char *s2);
+
+#endif
