ba9d3fd8c731b951650e4028690192f2e0ec51f6
tiida <takehi.iida@gmail.com>
2025-12-12 23:39:00 +0900

    ex03 ft_point.h

---
+++ ex03/ft_point.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_point.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiida <tiida@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/12 23:38:29 by tiida             #+#    #+#             */
+/*   Updated: 2025/12/12 23:38:31 by tiida            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_POINT_H
+# define FT_POINT_H
+
+struct	s_point
+{
+	int	x;
+	int	y;
+};
+typedef struct s_point	t_point;
+
+//void	set_point(t_point *point);
+#endif
